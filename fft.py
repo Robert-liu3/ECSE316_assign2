2a9eb40b726edfb5b825479200d43366fada3785
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2023-03-27 17:19:43 -0400

    Update fft.py

--- fft.py
+++ fft.py
@@ -1,1 +1,29 @@
-print("hello world")
+import sys
+
+
+#parse commands
+def main():
+    model = 1
+    print("hello world")
+    num_arg = len(sys.argv)
+    print("number of arguments " + str(num_arg))
+    match num_arg:
+        case 5:
+            model = sys.argv[1]
+            print(model)
+        case 3:  
+        case _: print("else")
+
+    # try:
+    if (sys.argv.index('-m') != None):
+        index = sys.argv.index('-m') 
+        mode = sys.argv[index + 1]
+        print(index)
+    else:
+        print("bruh")
+    # except:
+    #     print("An exception occured")
+        
+
+if __name__ == '__main__':
+    main()
